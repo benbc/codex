93f6961e7b4947aa1f2536aae7043fa76f496995
benbc <ben@bridesmere.com>
2010-09-10 16:40:53 +0100

    Add position to cells -- haskell.

--- haskell/life.hs
+++ haskell/life.hs
@@ -1,27 +1,29 @@
-{-# LANGUAGE EmptyDataDecls #-}
+import Test.HUnit hiding (State)
 
-import Test.HUnit
+data State = Alive | Dead deriving (Show, Eq)
+data Cell = Cell State Position deriving (Show)
 
-data Cell = Alive | Dead deriving (Show, Eq)
+alive, dead :: Cell -> Bool
+alive (Cell Alive _) = True
+alive (Cell Dead _) = False
+dead = not . alive
 
-alive :: Cell -> Bool
-alive Alive = True
-alive Dead = False
-
+data Position = Position deriving (Show)
 class Grid g where
-    neighbours :: g -> Cell -> [Cell]
-
-data XGrid
-instance Grid XGrid where
-    neighbours _ _ = undefined
+    neighbours :: g -> Position -> [Cell]
 
 nextGen :: Grid g => g -> Cell -> Cell
-nextGen g c | numAlive <  2 = Dead
-            | numAlive == 2 = c
-            | numAlive == 3 = Alive
-            | numAlive >  3 = Dead
-    where numAlive = numberAliveIn (neighbours g c)
+nextGen g (Cell state position) = Cell state' position
+    where state' | numAlive <  2 = Dead
+                 | numAlive == 2 = state
+                 | numAlive == 3 = Alive
+                 | numAlive >  3 = Dead
+          numAlive = numberAliveIn (neighbours g position)
           numberAliveIn = length . (filter alive)
+
+data XGrid = XGrid
+instance Grid XGrid where
+    neighbours g p = undefined
 
 {- TESTS -}
 
@@ -30,25 +32,29 @@ instance Grid CannedGrid where
     neighbours (CannedGrid ns) _ = ns
 
 noNeighbours = []
-twoNeigbours = replicate 2 Alive
-threeNeigbours = replicate 3 Alive
-fourNeigbours = replicate 4 Alive
+twoNeigbours = replicate 2 anAlive
+threeNeigbours = replicate 3 anAlive
+fourNeigbours = replicate 4 anAlive
 
-test1 = "dies with no neighbours" ~: Dead ~=? nextGen (CannedGrid noNeighbours) Alive
+anAlive = Cell Alive undefined
+aDead = Cell Dead undefined
+
+test1 = dead (ourNextGen anAlive) ~? "dies with no neighbours"
+    where ourNextGen = nextGen (CannedGrid noNeighbours)
 
 infix 1 `becomes`
-becomes = (~?=)
+becomes cell pred = pred cell ~? ""
 infix 2 `with`
 cell `with` neighbours = nextGen (CannedGrid neighbours) cell
 
-tests = [ Alive `with` noNeighbours `becomes` Dead
-        , Dead `with` noNeighbours  `becomes` Dead
-        , Alive `with` twoNeigbours `becomes` Alive
-        , Dead `with` twoNeigbours  `becomes` Dead
-        , Alive `with` threeNeigbours `becomes` Alive
-        , Dead `with` threeNeigbours  `becomes` Alive
-        , Alive `with` fourNeigbours `becomes` Dead
-        , Dead `with` fourNeigbours `becomes` Dead
+tests = [ anAlive `with` noNeighbours `becomes` dead
+        , aDead `with` noNeighbours  `becomes` dead
+        , anAlive `with` twoNeigbours `becomes` alive
+        , aDead `with` twoNeigbours  `becomes` dead
+        , anAlive `with` threeNeigbours `becomes` alive
+        , aDead `with` threeNeigbours  `becomes` alive
+        , anAlive `with` fourNeigbours `becomes` dead
+        , aDead `with` fourNeigbours `becomes` dead
         ]
 
 run = runTestTT $ test (test1:tests)
